c8ed0f59367b48d0845d181a5e18c2aceb0448ce
John Wei <41243908+johnwei417@users.noreply.github.com>
2020-02-26 17:31:15 -0500

    Update auth.guard.ts

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -5,7 +5,7 @@ import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from
 export class AuthGuard implements CanActivate {
 
     constructor(private router: Router) { }
-
+    
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         if (localStorage.getItem('currentUser')) {
             // logged in so return true
