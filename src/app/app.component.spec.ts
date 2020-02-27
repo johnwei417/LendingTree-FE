e5302215c3c2ea0ba26ef84f12763b682e3ca16f
John Wei <41243908+johnwei417@users.noreply.github.com>
2020-02-26 21:48:25 -0500

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,11 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'batch49-fe'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('batch49-fe');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
