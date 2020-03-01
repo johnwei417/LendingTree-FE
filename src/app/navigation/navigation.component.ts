6a9e73d70484906e74f67f7fabc0cbfceab1bc11
John Wei <41243908+johnwei417@users.noreply.github.com>
2020-02-29 20:49:33 -0500

    fixed navbar

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -12,12 +12,18 @@ import {User} from '../_models';
 export class NavigationComponent implements OnInit {
   currentUser: User;
   isLoggedIn$: Observable<boolean>;
+  isAdmin$: Observable<boolean>;
+  isCustomer$: Observable<boolean>;
+  isEmployee$: Observable<boolean>;
 
   constructor(private authenticationService: AuthenticationService) {
   }
 
   ngOnInit(): void {
     this.isLoggedIn$ = this.authenticationService.isLoggedIn;
+    this.isAdmin$ = this.authenticationService.isAdmin;
+    this.isCustomer$ = this.authenticationService.isCustomer;
+    this.isEmployee$ = this.authenticationService.isEmployee;
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
   }
 
